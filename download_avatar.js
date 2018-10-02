bd3bef1d5681d74a8d77ed7bf287ef35e650e5fb
Chinatriczeps <zhumagaliyev.chingiz@gmail.com>
2018-10-02 02:25:44 +0000

    Implemented function

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,18 @@
 var request = require('request');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb){
+
+};
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
