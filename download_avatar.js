794b845df7cd74d09454270a1339911018eaee2b
Chinatriczeps <zhumagaliyev.chingiz@gmail.com>
2018-10-02 04:21:58 +0000

    Solution for the one day assignment

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,5 @@
+var input1 = process.argv[2];
+var input2 = process.argv[3];
 var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets.js');
@@ -7,6 +9,9 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb){
+	if( repoOwner === undefined || repoName === undefined){
+		throw err;
+	} 
 	var options = {
 	url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
 	headers: {
@@ -25,7 +30,7 @@ function downloadImageByURL(url, filePath) {
   request.get(url).pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(input1, input2, function(err, result) {
   result.forEach(function(element){
 	var url = element.avatar_url;
 	var filePath = 'avatars/' + element.login + '.jpg';
